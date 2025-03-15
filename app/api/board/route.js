67f89eb8ab5520073fc09883e3e12224426aad06
Ramses Mendoza <mg.ramses@gmail.com>
2025-03-14 23:57:24 -0600

    new feature: dynamic pages and board deletion

--- app/api/board/route.js
+++ app/api/board/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
-import { auth } from "@/auth"
-import connectMongo from "@/libs/mongoose"
+import { auth } from "@/auth";
+import connectMongo from "@/libs/mongoose";
 import User from "@/models/User";
 import Board from "@/models/Board";
 
@@ -15,33 +15,62 @@ export async function POST(req) {
       );
     }
 
-    const session = await auth()
+    const session = await auth();
 
-    if(!session){
-        return NextResponse.json(
-            {error: "Not authorized"},
-            {status: 401}
-        )
+    if (!session) {
+      return NextResponse.json({ error: "Not authorized" }, { status: 401 });
     }
 
-    await connectMongo()
+    await connectMongo();
 
-    const user = await User.findById(session.user.id)
-    console.log("=== USER INFORMATION ===")
-    console.log(user)
-    console.log("=== USER INFORMATION ===")
+    const user = await User.findById(session.user.id);
+    console.log("=== USER INFORMATION ===");
+    console.log(user);
+    console.log("=== USER INFORMATION ===");
 
     const board = await Board.create({
-        userId: user._id,
-        name: body.name
+      userId: user._id,
+      name: body.name,
+    });
+
+    user.boards.push(board._id);
+    await user.save();
+
+    return NextResponse.json({});
+  } catch (error) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
+
+export async function DELETE(req) {
+  try {
+    const { searchParams } = req.nextUrl;
+    const boardId = searchParams.get("boardId");
+
+    if (!boardId) {
+      return NextResponse.json(
+        { error: "boardId is required" },
+        { status: 400 }
+      );
+    }
+
+    const session = await auth();
+
+    if (!session) {
+      return NextResponse.json({ error: "Not authorized" }, { status: 401 });
+    }
+
+    await Board.deleteOne({
+      _id: boardId,
+      userId: session?.user?.id
     })
 
-    user.boards.push(board._id)
-    await user.save()
+    const user= await User.findById(session?.user?.id)
+    user.boards.filter((id) => id.toString() !== boardId)
+    await user.save();
 
     return NextResponse.json({})
-
-  } catch (error) {
-    return NextResponse.json({error: error.message}, {status: 500})
+  } catch (e) {
+    return NextResponse.json({ error: e.message }, { status: 500 });
   }
 }
--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import ButtonLogout from "@/components/ButtonLogout";
 import FormNewBoard from "@/components/FormNewBoard";
 import { auth } from "@/auth";
@@ -26,15 +27,18 @@ export default async function Dashboard() {
 
         <div>
           <h1 className="font-extrabold text-xl mb-4">
-            {user.boards.length} Boards
+            {user.boards.length} Boards --
           </h1>
 
           <ul className="space-y-4">
             {user.boards.map((board) => {
               return (
-                <div key={board._id} className="bg-base-100 p-6 rounded-3xl">
-                  {board.name}
-                </div>
+                <li
+                  key={board._id}
+                  className="bg-base-100 p-6 rounded-3xl hover:bg-neutral hover:text-neutral-content duration-200"
+                >
+                  <Link href={`/dashboard/b/${board._id}`}>{board.name}</Link>
+                </li>
               );
             })}
           </ul>
